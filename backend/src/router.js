0fb2b7151c378153cf8726dd9d3f3dc963d0622a
Salamata Diallo <diallo.salamata2@gmail.com>
2023-11-06 12:00:07 +0100

    Upload fichier back et renommer nom du fichier

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,33 @@
 const express = require("express");
 
 const router = express.Router();
+const fs = require("fs");
+
+// Ajout de multer
+const multer = require("multer");
+// On définit la destination de stockage de nos fichiers
+// Ajout de uuid
+const { v4: uuidv4 } = require("uuid");
+
+const upload = multer({ dest: "./public/uploads/" });
+// route POST pour recevoir un fichier
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  // On récupère le nom du fichier
+  const { originalname } = req.file;
+
+  // On récupère le nom du fichier
+  const { filename } = req.file;
+
+  // On utilise la fonction rename de fs pour renommer le fichier
+  fs.rename(
+    `./public/uploads/${filename}`,
+    `./public/uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 const itemControllers = require("./controllers/itemControllers");
 
